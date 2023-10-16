0c9e6efd26b5551aacc2f9c2fe7e50ad6a3f3b1f
Andrija Žiković <126960888+andrija-zikovic@users.noreply.github.com>
2023-10-16 09:59:28 +0000

    modified:   client/.env 	modified:   client/.gitignore 	new file:   client/public/img/600x400_1697449919205-svinjski-but.webp 	modified:   server/.gitignore 	modified:   server/controllers/productsController.js 	modified:   server/logs/reqLog.txt 	modified:   server/package-lock.json 	modified:   server/package.json

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -1,4 +1,19 @@
 const Products = require('../model/Products');
+const multer = require('multer');
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/'); // specify the folder where files will be stored
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname); // generate a unique filename
+    }
+});
+
+const upload = multer({ storage: storage });
 
 const getAllProducts = async (req, res) => {
     try {
@@ -30,24 +45,61 @@ const getSpecificProducts = async (req, res) => {
 }
 
 const createProduct = async (req, res) => {
-    console.log(req.body.title);
-    /* if (!req?.body?.title || !req?.body?.price || !req?.body?.onStorage || !req?.body?.meatType || !req?.body?.imgSrc || !req?.bod?.image) {
-        return res.status(400).json({ 'message': 'Info (title, price, onStorage, meatType, imgSrc, image) is required.' })
-    }
-
     try {
-        const result = await Products.create({
-            title: req.body.title,
-            price: req.body.price,
-            onStorage: req.body.onStorage,
-            meatType: req.body.meatType,
-            imgSrc: req.body.imgSrc,
-        })
+        // Use Multer middleware to handle file uploads
+        upload.single('image')(req, res, async function (err) {
+            if (err instanceof multer.MulterError) {
+                // A Multer error occurred when uploading.
+                return res.status(500).json({ 'message': 'Multer error occurred.' });
+            } else if (err) {
+                // An unknown error occurred when uploading.
+                return res.status(500).json({ 'message': 'Unknown error occurred.' });
+            }
 
-        res.status(201).json(result);
-    } catch (err) {
-        console.error(err);
-    } */
+            console.log(req.file);
+            // At this point, the file is successfully uploaded.
+            const imagePath = req.file.path;
+            const clientImageFileName = `600x400_${req.file.filename}`;
+            const clientImagePath = path.join(__dirname, '..', '..', 'client', 'public', 'img', clientImageFileName);
+
+            try {
+                await sharp(imagePath)
+                    .resize(600, 400)
+                    .toFile(clientImagePath);
+            } catch (error) {
+                console.error('Error resizing image:', error);
+                return res.status(500).json({ 'message': 'Error resizing image.' });
+            }
+            
+            try {   
+                await fs.unlinkSync(imagePath);
+                console.log('File deleted successfully!');
+            } catch (error) {
+                console.error('Error deleting image:', error)
+                return res.status(500).json({'message': 'Error deleting image.'})
+            }
+
+            // Handle other form data or business logic here
+            
+            try {
+                const imgSrc = `img/600x400_${req.file.filename}`
+                const product = await Products.create({
+                    title: req.body.title,
+                    price: req.body.price,
+                    onStorage: req.body.onStorage,
+                    meatType: req.body.meatType,
+                    imgSrc: imgSrc
+                });
+                return res.status(201).json({'message': 'Product is added to products list!'});
+            } catch (error) {
+                console.error('Error adding a product:', error)
+                return res.status(500).json({'message': 'Error adding product to database'})
+            } 
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ 'message': 'Internal Server Error' });
+    }
 }
 
 module.exports = {
