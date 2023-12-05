8053ba3b9915b3fe775f9e04b213b20f520b4f25
Andrija Žiković <126960888+andrija-zikovic@users.noreply.github.com>
2023-12-05 13:21:56 +0000

    modified:   client/src/Client/orderForm.css 	modified:   client/src/Client/orderForm.js 	modified:   server/logs/reqLog.txt

--- client/src/Client/orderForm.js
+++ client/src/Client/orderForm.js
@@ -5,6 +5,11 @@ const OrderForm = ({ cartItems, deleteItem, clearCart }) => {
     console.log(cartItems);
     const [buyStatus, setBuyStatus] = useState(false);
     const formRef = useRef(null);
+    const [isLoadingVisible, setIsLoadingVisible] = useState(false);
+
+    const toggleLoadingVisibility = () => {
+        setIsLoadingVisible((prevState) => !prevState);
+    };
 
     const handlePriceCaluclation = (cartItem) => {
         if (cartItem.unit === 'dag') {
@@ -32,6 +37,7 @@ const OrderForm = ({ cartItems, deleteItem, clearCart }) => {
     }
 
     const handleOrderSend = async (cartItems, buyerData) => {
+        setIsLoadingVisible(true);
         try {
             const url = process.env.REACT_APP_ORDER_CALL_API;
             const res = await fetch(url, {
@@ -42,6 +48,7 @@ const OrderForm = ({ cartItems, deleteItem, clearCart }) => {
                 body: JSON.stringify({ "products": cartItems, "buyer": buyerData }), // Send cartItems as JSON
             });
             if (res.ok) {
+                setIsLoadingVisible(false);
                 setBuyStatus(true);
                 clearCart();
                 const data = await res.json();
@@ -84,6 +91,104 @@ const OrderForm = ({ cartItems, deleteItem, clearCart }) => {
 
         return (
             <main className='order'>
+                {isLoadingVisible && 
+                <div className='orderLoading'>
+                <svg
+                width="100"
+                height="80"
+                fill="#e33535cc"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                >
+                <style>
+                    {`
+                    .spinner_DupU {
+                        animation: spinner_sM3D 1.2s infinite;
+                    }
+                    .spinner_GWtZ { animation-delay: .1s; }
+                    .spinner_dwN6 { animation-delay: .2s; }
+                    .spinner_46QP { animation-delay: .3s; }
+                    .spinner_PD82 { animation-delay: .4s; }
+                    .spinner_eUgh { animation-delay: .5s; }
+                    .spinner_eUaP { animation-delay: .6s; }
+                    .spinner_j38H { animation-delay: .7s; }
+                    .spinner_tVmX { animation-delay: .8s; }
+                    .spinner_DQhX { animation-delay: .9s; }
+                    .spinner_GIL4 { animation-delay: 1s; }
+                    .spinner_n0Yb { animation-delay: 1.1s; }
+                    
+                    @keyframes spinner_sM3D {
+                        0%, 50% { animation-timing-function: cubic-bezier(0, 1, 0, 1); r: 0; }
+                        10% { animation-timing-function: cubic-bezier(.53, 0, .61, .73); r: 2px; }
+                    }
+                    `}
+                </style>
+                <circle class="spinner_DupU" cx="12" cy="3" r="0" />
+                <circle
+                    class="spinner_DupU spinner_GWtZ"
+                    cx="16.50"
+                    cy="4.21"
+                    r="0"
+                />
+                <circle
+                    class="spinner_DupU spinner_n0Yb"
+                    cx="7.50"
+                    cy="4.21"
+                    r="0"
+                />
+                <circle
+                    class="spinner_DupU spinner_dwN6"
+                    cx="19.79"
+                    cy="7.50"
+                    r="0"
+                />
+                <circle
+                    class="spinner_DupU spinner_GIL4"
+                    cx="4.21"
+                    cy="7.50"
+                    r="0"
+                />
+                <circle
+                    class="spinner_DupU spinner_46QP"
+                    cx="21.00"
+                    cy="12.00"
+                    r="0"
+                />
+                <circle
+                    class="spinner_DupU spinner_DQhX"
+                    cx="3.00"
+                    cy="12.00"
+                    r="0"
+                />
+                <circle
+                    class="spinner_DupU spinner_PD82"
+                    cx="19.79"
+                    cy="16.50"
+                    r="0"
+                />
+                <circle
+                    class="spinner_DupU spinner_tVmX"
+                    cx="4.21"
+                    cy="16.50"
+                    r="0"
+                />
+                <circle
+                    class="spinner_DupU spinner_eUgh"
+                    cx="16.50"
+                    cy="19.79"
+                    r="0"
+                />
+                <circle
+                    class="spinner_DupU spinner_j38H"
+                    cx="7.50"
+                    cy="19.79"
+                    r="0"
+                />
+                <circle class="spinner_DupU spinner_eUaP" cx="12" cy="21" r="0" />
+                </svg>
+                <h1>Šaljem naruđbu!</h1>
+                </div>
+                }
                 <table className='order__table'>
                     <thead className='order__thead'>
                         <tr>
