5b25711e60952f75084bc31f63b009fe3d4f77d0
Andrija Žiković <126960888+andrija-zikovic@users.noreply.github.com>
2023-12-07 15:46:02 +0000

    modified:   client/src/Admin/Admin.css 	modified:   client/src/Admin/AdminOrder.js 	modified:   client/src/Client/Header.css 	modified:   client/src/Client/Hero.css 	modified:   client/src/Client/Hero.js 	new file:   client/src/img/9c590853903501.59458ff923562.gif 	modified:   server/logs/reqLog.txt

--- client/src/Client/Hero.js
+++ client/src/Client/Hero.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Hero.css'
 import myImage from '../img/pexels-geraud-pfeiffer-6607314.jpg';
+import pig from '../img/9c590853903501.59458ff923562.gif';
 
 const Hero = () => {
     return (
         <section className='hero'>
             <h2 className='hero__h2' loading="lazy">Dobrodošli</h2>
             <figure>
-                <img src={myImage} alt='hero.jbg' width="1954" height="644" />
+                <img className="mainHero" src={myImage} alt='hero.jbg' width="1954" height="644" />
+                <img className="pig" src={pig} alt='pig.gif' width="442" height="442" />
                 <figcaption className='offscreen'>
                     Meat on plate
                 </figcaption>
