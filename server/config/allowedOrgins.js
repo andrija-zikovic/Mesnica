f5fbf1755918a923ac3e3754ba6870e34a1361f5
Andrija <andrija.zikovic@gmail.com>
2023-12-15 10:57:20 +0100

    modified:   client/.env 	modified:   client/package-lock.json 	modified:   client/package.json 	modified:   server/.gitignore 	modified:   server/config/allowedOrgins.js 	modified:   server/logs/reqLog.txt

--- server/config/allowedOrgins.js
+++ server/config/allowedOrgins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://127.0.0.1:5500',
+    'http://192.168.0.80:3000',
     'http://localhost:3500',
     'http://localhost:3000',
     'https://expert-fortnight-7v7xpprp4g4fwxr4-3000.app.github.dev',
