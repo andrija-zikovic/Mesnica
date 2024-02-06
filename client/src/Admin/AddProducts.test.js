ba855ffc0556535a6fe240c7a9c5f7f633ff6ec9
Andrija <andrija.zikovic@gmail.com>
2024-02-06 10:32:46 +0100

    modified:   client/src/Admin/AddProducts.test.js

--- client/src/Admin/AddProducts.test.js
+++ client/src/Admin/AddProducts.test.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { jest } from "@jest/globals";
 import { act } from "react-dom/test-utils";
 import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import AddProducts from "./AddProducts";
-import { DataAdminProvider } from "../context/DataAdmin";
+import DataAdmin from "../context/DataAdmin";
 
 describe("AddProducts", () => {
   test("renders without crashing", () => {
@@ -16,18 +17,20 @@ describe("AddProducts", () => {
 
   test("renders without error", () => {
     render(
-      <DataAdminProvider>
+      <DataAdmin.Provider
+        value={{
+          token: "someToken",
+          setLoading: () => {},
+          setReFetch: () => {},
+        }}
+      >
         <AddProducts />
-      </DataAdminProvider>
+      </DataAdmin.Provider>
     );
   });
 
   test("displays a form with input fields", () => {
-    const { getByLabelText } = render(
-      <DataAdminProvider>
-        <AddProducts />
-      </DataAdminProvider>
-    );
+    const { getByLabelText } = render(<AddProducts />);
 
     expect(getByLabelText("Name")).toBeInTheDocument();
     expect(getByLabelText("Price")).toBeInTheDocument();
@@ -38,9 +41,15 @@ describe("AddProducts", () => {
 
   test("updates state when input fields change", () => {
     const { getByLabelText } = render(
-      <DataAdminProvider>
+      <DataAdmin.Provider
+        value={{
+          token: "someToken",
+          setLoading: () => {},
+          setReFetch: () => {},
+        }}
+      >
         <AddProducts />
-      </DataAdminProvider>
+      </DataAdmin.Provider>
     );
 
     const productNameInput = getByLabelText("Name");
@@ -61,56 +70,55 @@ describe("AddProducts", () => {
 
   test("displays a submit button", () => {
     const { getByText } = render(
-      <DataAdminProvider>
+      <DataAdmin.Provider
+        value={{
+          token: "someToken",
+          setLoading: () => {},
+          setReFetch: () => {},
+        }}
+      >
         <AddProducts />
-      </DataAdminProvider>
+      </DataAdmin.Provider>
     );
 
     expect(getByText("ADD PRODUCT")).toBeInTheDocument();
   });
 
-  test("calls a submit function when submit button is clicked", () => {
+  test("calls a submit function when submit button is clicked", async () => {
     const submitMock = jest.fn();
     const { getByText } = render(
-      <DataAdminProvider>
+      <DataAdmin.Provider
+        value={{
+          token: "someToken",
+          setLoading: () => {},
+          setReFetch: () => {},
+        }}
+      >
         <AddProducts />
-      </DataAdminProvider>
+      </DataAdmin.Provider>
     );
 
     const form = document.querySelector("form");
     form.onsubmit = submitMock;
 
     const submitButton = getByText("ADD PRODUCT");
-    fireEvent.click(submitButton);
+    await act(async () => {
+      fireEvent.click(submitButton);
+    });
 
     expect(submitMock).toHaveBeenCalled();
   });
 
-  const mockContextValue = {
-    // Mock values and functions
-    token: jest.fn(),
-    setLoading: jest.fn(),
-    setReFetch: jest.fn(),
-  };
+  beforeAll(() => {
+    global.fetch = () =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({ message: "Product added successfully!" }),
+      });
+  });
 
-  jest.mock("react", () => ({
-    ...jest.requireActual("react"),
-    useContext: () => mockContextValue,
-  }));
-
-  const MockProvider = ({ children }) => (
-    <DataAdminProvider value={mockContextValue}>{children}</DataAdminProvider>
-  );
-
-  global.fetch = jest.fn(() =>
-    Promise.resolve({
-      ok: true,
-      json: () => Promise.resolve({ message: "Product Added" }),
-    })
-  );
-
-  beforeEach(() => {
-    fetch.mockClear();
+  afterEach(() => {
+    jest.restoreAllMocks();
   });
 
   beforeAll(() => {
@@ -121,14 +129,18 @@ describe("AddProducts", () => {
     global.URL.createObjectURL.mockRestore();
   });
 
-  test("testing a submit function", async () => {
-    const { getByText, getByLabelText } = render(
-      <MockProvider>
+  test("calls handleAddSubmit function when form is submitted", async () => {
+    const { getByLabelText, getByText } = render(
+      <DataAdmin.Provider
+        value={{
+          token: "someToken",
+          setLoading: () => {},
+          setReFetch: () => {},
+        }}
+      >
         <AddProducts />
-      </MockProvider>
+      </DataAdmin.Provider>
     );
-
-    fetch.mockImplementationOnce(() => Promise.resolve({ ok: true }));
 
     const productNameInput = getByLabelText("Name");
     const priceInput = getByLabelText("Price");
@@ -140,13 +152,15 @@ describe("AddProducts", () => {
     fireEvent.change(priceInput, { target: { value: "10.99" } });
     fireEvent.change(onStorageInput, { target: { value: "10" } });
     fireEvent.change(productTypeInput, { target: { value: "beef" } });
-    const file = new File(["(⌐□_□)"], "chucknorris.png", { type: "image/png" });
+    const file = new File(["(⌐□_□)"], "chucknorris.png", {
+      type: "image/png",
+    });
     fireEvent.change(imgSrcInput, { target: { files: [file] } });
 
-    const submitButton = getByText("ADD PRODUCT");
+    const form = document.querySelector("form");
 
-    await act(() => {
-      fireEvent.click(submitButton);
+    await act(async () => {
+      fireEvent.submit(form);
     });
 
     await waitFor(() => {
