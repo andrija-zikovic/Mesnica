b7df424c00e72cbc30d822ccbcc43a7848cd6c7b
Andrija <andrija.zikovic@gmail.com>
2024-02-07 13:12:50 +0100

    Adding origin

--- server/config/allowedOrgins.js
+++ server/config/allowedOrgins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://192.168.0.80:3000",
   "http://localhost:3500",
   "http://localhost:3000",
+  "http://localhost",
   "https://expert-fortnight-7v7xpprp4g4fwxr4-3000.app.github.dev",
   "https://m6vbzp7p-3000.euw.devtunnels.ms",
 ];
